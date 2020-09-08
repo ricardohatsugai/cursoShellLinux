488b7befa1bbad57053a5ec14c42a598e2b45873
Ricardo Hatsugai <rhatsugai@gmail.com>
2020-09-07 22:14:29 -0300

    20200907 conclusão do curso

--- Shell_Script/mod10/sistema_de_usuarios2.sh
+++ Shell_Script/mod10/sistema_de_usuarios2.sh
@@ -26,36 +26,23 @@
 # ------------------------------- VARIÁVEIS ----------------------------------------- #
 ARQUIVO_BANCO_DE_DADOS="banco_de_dados.txt"
 SEP=:
-TEMP="\temp.$$"
+TEMP=temp.$$
 VERDE="\033[32;1m"
 VERMELHO="\033[31;1m"
 # ------------------------------------------------------------------------ #
 
 # ------------------------------- TESTES ----------------------------------------- #
 [ ! -e "$ARQUIVO_BANCO_DE_DADOS" ] && echo "ERRO. Arquivo não existente." && exit l
-[ ! -r "$ARQUIVO_BANCO_DE_DADOS" ] && echo "ERRO. Arquivo não tem permissão de leitura." && exit
+[ ! -r "$ARQUIVO_BANCO_DE_DADOS" ] && echo "ERRO. Arquivo não tem permissão de leitura." && exit 1
 [ ! -w "$ARQUIVO_BANCO_DE_DADOS" ] && echo "ERRO. Arquivo não tem permissão de escrita." && exit 1
+[ ! -x "$(which dialog)" ] && sudo apt install dialog 1> /dev/null 2>&1
 # ------------------------------------------------------------------------ #
 
 # ------------------------------- FUNÇÕES ----------------------------------------- #
-MostraUsuarioNaTela () {
-  local id="$(echo $linha | cut -d $SEP -f 1)"
-  local nome="$(echo $linha | cut -d $SEP -f 2)"
-  local email="$(echo $linha | cut -d $SEP -f 3)"
-
-  echo -e "${VERDE}ID: ${VERMELHO}$id"
-  echo -e "${VERDE}Nome: ${VERMELHO}$nome"
-  echo -e "${VERDE}E-mail: ${VERMELHO}$email"
-}
-
 ListaUsuarios () {
-  while read -r linha
-  do
-    [ "$(echo $linha | cut -c1)" = "#" ] && continue
-    [ ! "$linha" ] && continue
-
-    MostraUsuarioNaTela "$linha"
-  done < "$ARQUIVO_BANCO_DE_DADOS"
+  egrep -v "^#|^$" banco_de_dados.txt | tr : ' ' > "$TEMP"
+  dialog --title "Lista de Usuários" --textbox "$TEMP" 20 40
+  rm -f "$TEMP"
 }
 
 ValidaExistenciaUsuario () {
@@ -63,15 +50,21 @@ ValidaExistenciaUsuario () {
 }
 
 InsereUsuario () {
-  local nome="echo $1 | cut -d $SEP -f 2"
+  local ultimo_id="$(egrep -v "^#|^$" banco_de_dados.txt | sort | tail -n 1 | cut -d $SEP  -f 1)"
+  local proximo_id=$(($ultimo_id+1))
 
-  if ValidaExistenciaUsuario "$nome"
-  then
-    echo "ERRO. Usuário já existe!" && exit 1
-  else
-    echo "$*" >> "$ARQUIVO_BANCO_DE_DADOS"
-    echo "Usuário cadastrado com sucesso!"
-  fi
+  local nome=$(dialog --title "Cadastro de Usuários" --stdout --inputbox "Digite o seu nome" 0 0)
+  ValidaExistenciaUsuario "$nome" && {
+    dialog --title "ERRO FATAL!" --msbox "Usuário já cadastrado no sistema!" 6 40
+    exit 1
+  }
+
+  local email=$(dialog --title "Cadastro de Usuário" --stdout --inputbox "Digite o seu E-Mail" 0 0)
+
+  echo "$proximo_id$SEP$nome$SEP$email" >> "$ARQUIVO_BANCO_DE_DADOS"
+  dialog --title "Sucesso!" --msgbox "usuário cadastrado com sucesso!" 6 40
+
+  ListaUsuarios
 }
 
 ApagaUsuario () {
@@ -87,8 +80,34 @@ OrdenaLista () {
   sort "$ARQUIVO_BANCO_DE_DADOS" > "$TEMP"
   mv "$TEMP" "$ARQUIVO_BANCO_DE_DADOS"
 }
+
+RemoverUsuario () {
+  local usuarios=$(egrep "^#|^$" -v "$ARQUIVO_BANCO_DE_DADOS" | sort -h | cut -d $SEP -f 1,2 | sed 's/:/ "/;s/$/"/')
+  local id_usuario=$(dialog --stdout --menu "Escolha um usuário:" 0 0 0 $usuarios)
+
+  grep -i -v "^$id_usuario$SEP" "$ARQUIVO_BANCO_DE_DADOS" > "$TEMP"
+  mv "$TEMP" "$ARQUIVO_BANCO_DE_DADOS"
+
+  dialog -- msgbox "Usuário removido com sucesso!"
+  ListaUsuarios
+}
 # ------------------------------------------------------------------------ #
 
 # ------------------------------- EXECUÇÃO ----------------------------------------- #
+while :
+do
+  acao=$(dialog --title "Gerenciamento de Usuários 2.0" \
+                --stdout \
+                --menu "Escolha uma das opções abaixo:" \
+                0 0 0 \
+                listar "Listar todos os usuários do sistema" \
+                remover "Remover um usuário do sistema" \
+                inserir "Inserir um novo usuário no sistema")
 
+  case $acao in
+    listar) ListaUsuarios ;;
+    inserir) InsereUsuario ;;
+    remover) RemoverUsuario ;;
+  esac
+done
 # ------------------------------------------------------------------------ #
